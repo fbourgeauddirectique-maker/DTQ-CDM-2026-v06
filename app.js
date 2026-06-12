49b5f9a3a46f27f5fed1671163834e0e48eeb9b3
fbourgeauddirectique-maker <fbourgeauddirectique@gmail.com>
2026-06-12 09:42:07 +0200

    Update app.js
    Changement du nombre de points attribués

--- app.js
+++ app.js
@@ -384,7 +384,7 @@ function getStatsForUser(uid) {
 }
 
 function scorePrediction(pred, match) {
-  if (pred.home === match.homeScore && pred.away === match.awayScore) return 3;
+  if (pred.home === match.homeScore && pred.away === match.awayScore) return 5;
   return getOutcome(pred.home, pred.away) === getOutcome(match.homeScore, match.awayScore) ? 1 : 0;
 }
 
